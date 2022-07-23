402dfcf59783413cd29fff1f598b8b6e37a7bfd0
Chris Moss <hcmoss70@gmail.com>
2022-07-23 16:51:57 -0400

    added data to graph

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -27,3 +27,32 @@ export function getLabels(transaction){
 
   return percent
 }
+
+export function chartData(transaction, custom){
+
+    let bg = _.map(transaction, a=> a.color)
+    bg = _.uniq(bg)
+
+    let dataValue = getSum(transaction)
+
+    const config = {
+        data:{
+            datasets: [{
+                data: dataValue,
+                backgroundColor: bg,
+                hoverOffset: 4,
+                borderRadius: 30,
+                spacing: 10
+              }]
+        },
+        options:{
+            cutout:115
+        }
+      }
+
+return custom ?? config
+}
+
+export function getTotal(transaction){
+    return _.sum(getSum(transaction))
+}
